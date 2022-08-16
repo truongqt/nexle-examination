7795617887f2629c6fac871fd9979902ee8485fc
Admin <nguyenvantruongqt@gmail.com>
2022-08-16 10:09:49 +0700

    Test main

--- src/screens/Starting/StartingScreen.tsx
+++ src/screens/Starting/StartingScreen.tsx
@@ -1,3 +1,4 @@
+// main
 import {CommonActions, useNavigation} from '@react-navigation/native';
 import {StackNavigationProp} from '@react-navigation/stack';
 import {DEFAULT_HEADERS} from 'apisauce';
